aa07100c69d93c385413ee0b34cbb9786ff5ceda
niker68 <61888868+niker68@users.noreply.github.com>
2021-04-20 14:57:24 +0500

    war added

--- src/main/java/utils/HibernateSessionFactoryUtil.java
+++ src/main/java/utils/HibernateSessionFactoryUtil.java
@@ -6,12 +6,20 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+import java.io.File;
+
 public class HibernateSessionFactoryUtil {
     private static SessionFactory sessionFactory;
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
+                File file = new File("");
                 Configuration configuration = new Configuration().configure();
+
+                //jdbc:sqlite:C:/Users/PC home/IdeaProjects/MoscowExchange/db/db_MoscowExchange
+                String str= "jdbc:sqlite:"+file.getAbsolutePath()+"/db/db_MoscowExchange";
+                System.out.println(str);
+                configuration.setProperty("hibernate.connection.url",str);
                 configuration.addAnnotatedClass(Security.class);
                 configuration.addAnnotatedClass(History.class);
                 StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
